1191b2acd82155de363b673a96557d98c53652a1
rasikhikram <rasikhikram06@gmail.com>
2024-10-25 21:02:35 +0500

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,191 @@
+<template>
+  <nav class="flex items-center justify-between max-w-4xl mx-auto py-5">
+    <img src="@/assets/logo.png" class="w-30">
+    <ul class="flex items-center space-x-5">
+      <li><a href="" class="text-black">About</a></li>
+      <li><a href="" class="text-black">Jobs</a></li>
+      <li><a href="" class="text-black">Careers</a></li>
+      <li><a href="" class="text-black">Blogs</a></li>
+      <li><a href="" class="text-black">Sign In</a></li>
+      <button class="bg-purple-600 text-white py-2 px-4 rounded-lg">Get Started</button>
+    </ul>
+  </nav>
+
+  <header class="text-center mt-10 md:px-0 px-4">
+    <h3 class="text-[#00142E] md:text-3xl text-xl font-medium">Get All The Live Event</h3>
+    <h1 class="text-[#00142E] md:text-5xl text-3xl font-semibold ">
+      Insights <img src="@/assets/Mask group.png" class="inline w-10 h-12 mb-[-8px]"> You Need
+    </h1>
+    <p class="text-[#3F464B] md:text-base leading-5 mt-2 text-sm">
+      Our AI-powered market intelligence platform empowers you to make informed <br> decisions and confidently navigate the live event landscape.
+    </p>
+    <button class="bg-purple-600 text-white py-2 px-4 mt-4 rounded-lg">Join Now</button>
+    <div class="flex items-center justify-center">
+      <img src="@/assets/Safari (Big Sur) - Light.png" class="w-[865px] mt-8">
+    </div>
+  </header>
+  <section class="text-center mt-10 ">
+    <h1 class="text-[#00142E] text-3xl font-semibold">A powerful platform <br> that works for you</h1>
+
+    <div class="flex justify-between space-x-5 max-w-4xl mx-auto mt-12">
+      <div class="max-w-xs">
+        <img src="@/assets/Frame 15.png">
+        <h2 class="text-left text-lg font-semibold text-black mt-4">Unprecedented Data Set</h2>
+        <p class="text-left text-xs text-[#3F464B] mt-2">
+          Don't waste hours searching on different tabs. Sokratiq unifies primary, secondary, and historical market data all in one platform.
+        </p>
+      </div>
+      <div class="max-w-xs">
+        <img src="@/assets/Frame 15 (1).png">
+        <h2 class="text-left text-lg font-semibold text-black mt-4">AI-Powered Foresight</h2>
+        <p class="text-left text-xs text-[#3F464B] mt-2">
+          Outsmart the market with AI that analyzes real-time trends and historical data. Identify hidden opportunities and predict consumer behavior shifts before your competitors.
+        </p>
+      </div>
+      <div class="max-w-xs">
+        <img src="@/assets/Layer_3.png">
+        <h2 class="text-left text-lg font-semibold text-black mt-4">Eliminate Blindspots</h2>
+        <p class="text-left text-xs text-[#3F464B] mt-2">
+          Gain a live pulse on the market. Our real-time insights help you react to ticket trends as they happen.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="text-center mt-12">
+    <div class="flex justify-between space-x-12 mx-auto items-center max-w-4xl">
+      <div class="text-left max-w-lg">
+        <h1 class="text-[#00142E] text-3xl font-semibold max-w-[300px]">Revolutionising <br> The Live Events Ticketing Industry</h1>
+        <p class="text-sm text-[#3F464B] mt-4 max-w-[300px]">
+          Information can be overwhelming, especially when looking at the primary and secondary markets. We've spent years refining our AI so you can easily access your ticketing insights, all aimed at maximizing your profits on ticket sales.
+        </p>
+      </div>
+      <div>
+        <img src="@/assets/Frame 23.png" class="w-[580px] h-[400px]">
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-[#00142E] text-white text-center py-12 mt-10 rounded-lg mx-10">
+    <h1 class="text-3xl font-semibold">Sokratiq</h1>
+    <p class="mt-4 text-base">
+      Lorem ipsum dolor sit amet consectetur. Sed lorem dui congue <br> cursus malesuada ac non. Nunc auctor suscipit vitae integer.
+    </p>
+
+    <div class="flex space-x-16 items-center max-w-4xl mx-auto mt-12">
+      <div class="p-8">
+        <h1 class="text-5xl bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent">1B</h1>
+        <p>Market Movements tracked</p>
+      </div>
+      <div class="bg-purple-600 p-8 rounded-lg">
+        <h1 class="text-5xl bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent">12,427,125</h1>
+        <p>Row of data extracted</p>
+      </div>
+      <div class="p-8">
+        <h1 class="text-5xl bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent">200K</h1>
+        <p>Events Tracked</p>
+      </div>
+    </div>
+    <img src="@/assets/Bottom.png" class="mb-6 ml-[180px] max-w-4xl">
+ 
+
+    <div class="flex justify-between space-x-12 mx-auto items-center max-w-4xl mt-8">
+      <div class="text-left max-w-lg">
+        <h1 class="text-white text-3xl font-semibold max-w-[300px]">We're A Value-Led Company</h1>
+        <p class="text-xs text-white mt-4 max-w-[300px] text-[#F0F0F0]">
+          Since our launch, we've been obsessed with helping you win in the ticketing game. Our values have helped carve our culture and reinforce our relationship with our stakeholders.
+        </p>
+        <button class="bg-purple-600 text-white py-2 px-4 mt-4 rounded-lg">Get Started</button>
+      </div>
+      <div>
+        <img src="@/assets/Safari (Big Sur) - Light2.png" class="w-[580px] h-[400px]">
+      </div>
+    </div>
+    <div class="flex justify-between space-x-5 max-w-4xl mx-auto mt-12">
+      <div class="max-w-xs">
+        <img src="@/assets/Frame 15.png">
+        <h2 class="text-left text-lg font-semibold text-white mt-4">Trust</h2>
+        <p class="text-left text-xs text-[#F0F0F0] mt-2">
+          Don't waste hours searching on different tabs. Sokratiq unifies primary, secondary, and historical market data all in one platform.
+        </p>
+      </div>
+      <div class="max-w-xs">
+        <img src="@/assets/Frame 15 (1).png">
+        <h2 class="text-left text-lg font-semibold text-white mt-4">Customer Success</h2>
+        <p class="text-left text-xs text-[#F0F0F0] mt-2">
+          Outsmart the market with AI that analyzes real-time trends and historical data. Identify hidden opportunities and predict consumer behavior shifts before your competitors.
+        </p>
+      </div>
+      <div class="max-w-xs">
+        <img src="@/assets/Layer_3.png">
+        <h2 class="text-left text-lg font-semibold text-white mt-4">Innovation</h2>
+        <p class="text-left text-xs text-[#F0F0F0] mt-2">
+          Gain a live pulse on the market. Our real-time insights help you react to ticket trends as they happen.
+        </p>
+      </div>
+    </div>
+    <img src="@/assets/Bottom.png" class="mt-6 mb-[-50px]">
+  </section>
+
+  <section class="max-w-lg mx-auto mt-12">
+    <div class="text-[#00142E] text-center">
+      <h1 class="text-3xl font-semibold">Get in contact</h1>
+      <p class="text-sm mt-2"> Unlock the future of market insights with advanced modelling. <br> Join our waitlist for early access </p>
+    </div>
+      <div class="p-4 mt-2">
+        <div class="flex space-x-4">
+          
+          <div class="flex-1">
+            <input type="email" id="email" placeholder="Email" class="w-full px-3 py-1 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:border-transparent bg-gray-200 text-black">
+          </div>
+
+          <div class="flex-1">
+            <input type="tel" id="phone" placeholder="Phone Number" class="w-full px-3 py-1 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:border-transparent bg-gray-200 text-black">
+          </div>
+        </div>
+
+        <div class="mb-4 mt-4">
+          <input type="text" id="name" placeholder="Name" class="w-full px-3 py-1 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:border-transparent bg-gray-200 text-black">
+        </div>
+        <button class="bg-purple-600 text-white py-1 w-full mt-1 rounded-lg">Submit</button>
+      </div>
+      
+  </section>
+
+  <footer class="flex justify-between items-start py-8 mb-2 w-full border-t border-b border-gray-300 mx-auto max-w-[865px] mt-8">
+  <div class="flex flex-col space-y-4 max-w-[350px] text-sm text-gray-800">
+    <img src="@/assets/logo.png" class="w-32 h-8">
+    <p class="text-[#3F464B]">
+      Where ticket brokers defy doubt. Our market intelligence platform powered by AI empowers you to make informed decisions and confidently navigate the ticketing landscape.
+    </p>
+    <p class="font-bold">
+      <i class="fa-solid fa-envelope"></i> contact@sokratiq.com
+    </p>
+  </div>
+  <div class="space-y-1 text-black">
+    <h3 class="font-semibold">Quick Links</h3>
+    <p class="cursor-pointer">About</p>
+    <p class="cursor-pointer">Jobs</p>
+  </div>
+  <div class="space-y-1 text-black">
+    <h3 class="font-semibold">Resources</h3>
+    <p class="cursor-pointer">Careers</p>
+  </div>
+  <div class="space-y-8 text-black">
+    <i class="fa-brands fa-facebook block"></i>
+    <i class="fa-brands fa-square-instagram block mt-4"></i>
+    <i class="fa-brands fa-x-twitter block"></i>
+    <i class="fa-brands fa-linkedin block"></i>
+  </div>
+</footer>
+
+<div class="flex justify-between items-center text-[#3F464B] text-sm w-full max-w-[865px] mx-auto mb-4">
+  <div class="flex justify-between space-x-8">
+    <p class="cursor-pointer">Privacy Policy</p>
+    <p class="cursor-pointer">Terms & Conditions</p>
+  </div>
+  <div>
+    <p>© 2024 Sokratiq. All rights reserved.</p>
+  </div>
+</div>
+</template>
